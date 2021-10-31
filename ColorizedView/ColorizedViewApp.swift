b5f1babe8e09cc6678a930e793cf600c46d27482
Igor Buzykin <lexemz@gmail.com>
2021-10-31 18:18:47 +0300

    Initial Commit

---
+++ ColorizedView/ColorizedViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedViewApp.swift
+//  ColorizedView
+//
+//  Created by Igor on 31.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
